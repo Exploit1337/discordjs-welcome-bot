a8d709d0a8caa7275ed41d102fc75be719178f66
Exploit1337 <44264683+Exploit1337@users.noreply.github.com>
2019-03-15 01:21:51 -0400

    Add files via upload

---
+++ commands/setchannel.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+var jimp = require('jimp');
+const db = require("quick.db")
+
+module.exports.run = async (bot, message, args) => {
+ //set perms here
+ let cArgs = args[0]
+ 
+ if(isNaN(cArgs)) return message.channel.send("You must specify a valid id for the welcome channel!")
+	 
+ try{
+	 bot.guilds.get(message.guild.id).channels.get(cArgs).send("Welcome channel set!")
+	 
+ db.set(`${message.guild.id}`, cArgs)
+ 
+ message.channel.send("You have successfully set the welcome channel to <#" + cArgs + ">")
+return;
+ }catch(e){
+	return message.channel.send("Error: missing permissions or channel doesn't exist")
+ }
+ 
+ 
+}
+module.exports.help = {
+  name: "channel"
+}
